fcad2ec21f468c32110b9c9483e10631f8e80875
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-14 00:47:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ let response;
 function scuberGreetingForFeet(dist) {
   if(dist <= 400) {
     return 'This one is on me!';
+  } else if(dist > 2000) {
+    return 'I will gladly take your thirty bucks.'
   }
 }
