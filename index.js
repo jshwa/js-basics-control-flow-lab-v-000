5126f7caffece3f7f2d48087e27b643c509bcca7
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-14 00:40:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(dist) {
-  switch (dist) {
-    case dist <= 400:
-      'This one is on me!';
-      break;
+  if(dist <= 400) {
+    'This one is on me!'
   }
 }
