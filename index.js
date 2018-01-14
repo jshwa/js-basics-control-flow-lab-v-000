409af9e3e40486accf4d1daab4fa7a482633df20
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-14 00:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Write your code in this file!
+
+const response
+
 function scuberGreetingForFeet(dist) {
   if(dist <= 400) {
-    'This one is on me!';
+    response = 'This one is on me!';
   }
 }
